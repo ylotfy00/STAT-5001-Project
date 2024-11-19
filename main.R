4399611741a5e263f0afe99b70d65c2995dab03e
ylotfy00 <ylotfy00@gmail.com>
2024-11-19 15:53:04 -0600

    Packages

--- main.R
+++ main.R
@@ -1,2 +1,5 @@
+
+if(!exists("install_packages",mode="function"))source("packages.R")
+
 data = read.csv("data1.csv")
 head(data)
---
+++ packages.R
@@ -0,0 +1,48 @@
+install_packages = function(){
+
+if (!require("TDA")) install.packages("TDA")
+library(TDA)
+
+if (!require("imager")) install.packages("imager")
+library(imager)
+
+if (!require("vegan")) install.packages("vegan")
+library(vegan)
+
+if (!require(e1071)) install.packages("e1071")
+library(e1071)
+
+if (!require("RDRToolbox")) install.packages("RDRToolbox")
+library(RDRToolbox)
+
+if (!require("rgl")) install.packages("rgl")
+library(rgl)
+
+if (!require("tseries")) install.packages("tseries")
+library(tseries)
+
+if (!require("remotes")) install.packages("remotes")
+
+if (!require("devtools")) install.packages("devtools")
+
+if (!require("tseries")) install.packages("tseries")
+
+library(tseries)
+
+if (!require("plyr")) install.packages("plyr")
+
+library(plyr)
+
+if (!require("purrr")) install.packages("purrr")
+
+library(purrr)
+
+if (!require("igraph")) install.packages("igraph")
+library(igraph)
+
+if (!require("igraphdata")) install.packages("igraphdata")
+library(igraphdata)
+
+if (!require("TDA")) install.packages("TDA")
+library(TDA)
+}
