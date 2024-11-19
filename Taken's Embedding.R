e3a566fa609c193312914ae027753f5875475ebf
ylotfy00 <ylotfy00@gmail.com>
2024-11-19 16:53:48 -0600

    takensv2

--- Taken's Embedding.R
+++ Taken's Embedding.R
@@ -1,58 +1,18 @@
 ########################################################
 # Persistent Homology for Time Series                  #
 ########################################################
-if (!require("tseries")) install.packages("tseries")
 
-library(tseries)
-
-if (!require("plyr")) install.packages("plyr")
-
-library(plyr)
-
-if (!require("purrr")) install.packages("purrr")
-
-library(purrr)
-
-births <- scan("nybirths.dat")
-
-birthstimeseries <- ts(births, frequency=12, start=c(1946,1))
-plot.ts(birthstimeseries)
-
-birthstimeseriescomponents <- decompose(birthstimeseries)
-
-
-
-plot(birthstimeseriescomponents)
-
-plot(birthstimeseriescomponents$random)
-
-
-
-opar <- par(mfrow=c(1,2), mex=0.6, mar=c(5,4,3,2)+.3)
-
-acf(birthstimeseries,lag.max=20)
-
-acf(birthstimeseriescomponents$random,na.action=na.pass,lag.max=20)
-
-par(opar)
-
-
+data = read.csv("data1.csv")
 ########################################################
 # Takens Embedding                                     #
 ########################################################
-n = 480
+n = 100
 d = 2
 
-period = 12
-ts1 = cos(1:n*2*pi/period)
+cab_roll = data[2]
 
-period = 48
-ts2 = cos(1:n*2*pi/period)
 
-period = 96
-ts3 = cos(1:n*2*pi/period)
-
-ts = ts3
+ts = cab_roll
 tau = which(abs(acf(ts,plot=F)$acf) < 2/sqrt(n))[1]-1
 Takens=t(purrr::map_dfc(1:(n-(d-1)*tau),~ts[seq(from=.x,by=tau,length.out=d)]))
 diag=ripsDiag(Takens, maxdimension=1, maxscale=max(dist(Takens)))
