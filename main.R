40eca9c465f0095039028bc86faf4b3820a055c1
ylotfy00 <ylotfy00@gmail.com>
2024-11-26 16:47:40 -0500

    adding other subsets of the data

--- main.R
+++ main.R
@@ -1,5 +1,17 @@
 install_packages()
 
+transportation <- c(0,1,7,8,9)
+
+energy_sectors <- c(2,3,4,6)
+
+water_sectors <- c(5,10,11)
+
+#transportation
 PPP = read.csv('PPP data Encoded.csv')    
 
-PPP_full = read.csv('PPP Full data encoded.csv')
+#Everything
+PPP_full = read.csv('Encoded FUll Data.csv')
+
+energy_PPP <- PPP_full[PPP_full$Sector %in% energy_sectors, ]
+
+water_PPP <- read.csv('Water PPP encoded.csv')
